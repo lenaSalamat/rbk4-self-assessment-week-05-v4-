11f427565a192296679ca761591b3a8ff06d3375
lenaSalamat <lenasalamat96@gmail.com>
2018-04-14 09:52:08 +0300

    last

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -2,13 +2,12 @@ var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
-var db=require('/node-express-sequelize.index.js')
-var users = express.Router('/users');
+var users=require('/node-express-sequelize.index.js');
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 app.get('/users',function(req,res){
-db.User.find(users,then(function(err,data)){
+users.User.find(username,then(function(err,data)){
 	if(err){
 	console.log(err);	
 	}else{
