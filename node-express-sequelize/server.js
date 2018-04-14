87f65711bc5b845ce287cff4a0adbca1b099a56a
lenaSalamat <lenasalamat96@gmail.com>
2018-04-14 09:31:59 +0300

    last commit

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -2,11 +2,20 @@ var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
-
+var db=require('/node-express-sequelize.index.js')
+var users = express.Router('/users');
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
-
+app.get('/users',function(req,res){
+db.User.find(users,then(function(err,data)){
+	if(err){
+	console.log(err);	
+	}else{
+	res.send(data);	
+	}
+})
+});
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
 
